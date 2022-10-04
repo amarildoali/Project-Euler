e39a2112d95635b958618adb9f61873874d0879f
Amarildo Aliaj <82412439+amarildoaliaj@users.noreply.github.com>
2022-10-04 15:24:47 +0200

    refactor: Rename variable

--- src/P002.py
+++ src/P002.py
@@ -1,8 +1,9 @@
 from src import Algorithms
 
-x = []
+result = []
 for i in range(34):  # F_34 exceeds 4 million
-    f = Algorithms.fibo(i)
+    f = Algorithms.fibo(i)  # O(n)
     if (f % 2) == 0:
-        x.append(f)
-print(sum(x))
+        result.append(f)
+print(f"Solution = {sum(result)}")
+print("Complexity = O(n^2)")
